fd277c8f5b04668b662e0d57f0596cabf6322f95
Manideep79 <alupulamanideep@gmail.com>
2024-04-24 11:39:18 +0530

    newdb file update runs in locally

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -1,10 +1,23 @@
+const mysql = require('mysql');
+
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'password',
+  database: 'reactjswebapplication',
+});
+
+module.exports = pool;
+
+
 // // config/database.js
 // const mysql = require('mysql');
 
 // // Create a MySQL connection pool
 // const pool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
+//   host: 'cwsdatabase.c5yqu4yg2dxu.eu-north-1.rds.amazonaws.com',
+//   user: 'cwsngo',
 //   password: 'password',
 //   database: 'reactjswebapplication',
 // });
@@ -13,17 +26,3 @@
 
 
 
-
-
-// config/database.js
-const mysql = require('mysql');
-
-// Create a MySQL connection pool
-const pool = mysql.createPool({
-  host: 'cwsdatabase.c5yqu4yg2dxu.eu-north-1.rds.amazonaws.com',
-  user: 'cwsngo',
-  password: 'password',
-  database: 'reactjswebapplication',
-});
-
-module.exports = pool;
--- src/server/server.js
+++ src/server/server.js
@@ -1,25 +1,9 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const authRoutes = require('./routes/authRoutes');
-// const app = express();
-// const port = 5000;
-
-// app.use(bodyParser.json());
-// app.use('/auth', authRoutes);
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');  // Import the cors module
+const cors = require('cors');  
 const authRoutes = require('./routes/authRoutes');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(cors());  // Enable CORS for all routes
 app.use(bodyParser.json());
